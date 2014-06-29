05be77f757db353a5fa0a3925486a6fe39490184
Andre Anjos <andre.dos.anjos@gmail.com>
2014-06-29 12:31:58 +0200

    Django 1.6 compatibility changes

--- flatties/urls.py
+++ flatties/urls.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python
 # vim: set fileencoding=utf-8 :
 # Andre Anjos <andre.dos.anjos@gmail.com>
-# Mon 19 Apr 2010 05:57:29 PM CEST 
+# Mon 19 Apr 2010 05:57:29 PM CEST
 
 """URLs for flat pages
 """
 
-from django.conf.urls.defaults import *
+from django.conf.urls import patterns, url
 from flatties.views import *
 
 urlpatterns = patterns('',
 
   url(r'^(?P<url>\w[-\w/]+\w)/$', view_page, name='view'),
-  
+
 )
 
 namespaced = (urlpatterns, 'flatties', 'flatties')
